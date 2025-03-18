9db92d3432b76360f1129f3fa76aae8f9015e322
enjore <shawn630730@gmail.com>
2025-03-18 15:01:39 +0700

    ✨ 관리자 기능 구현
    재고에 붕어빵을 채워넣을 수 있는 기능 구현

--- fishbread.py
+++ fishbread.py
@@ -1,10 +1,49 @@
 
+stock = {
+    '팥붕어빵': 10,
+    '슈크림붕어빵': 8,
+    '초코붕어빵': 5
+    }
+
+sales = {
+    '팥붕어빵': 0,
+    '슈크림붕어빵': 0,
+    '초코붕어빵': 0
+}
+
 def order_bread():
-    print("안녕")
+    while True:
+        bread_type = input("주문할 붕어빵을 선택하세요 (팥붕어빵, 슈크림붕어빵, 초코봉붕어빵) 똥는 '뒤로가기'입력 :")
+        if bread_type == "뒤로가기":
+            break
+            #메뉴 주문+
+        if bread_type in stock:
+            bread_count = int(input("주문할 개수를 입력하새요"))
+            if stock[bread_type] >= bread_count:
+                stock[bread_type] -= bread_count
+                sales[bread_type] += bread_count
+                print(f'{bread_type}을 {bread_count}개 판매했습니다')
+            else:
+                print(f'재고가 부족합니다, 현재{stock[bread_type]}개만 주문가능합니다')
+        else:
+            print("팥붕어빵, 슈크림붕어빵, 초코봉붕어빵 중 하나의 맛을 선택해주세요")
 
-def admin_bread():
-    print("안녕 점주")
 
+
+#관리자 모드
+def admin_mode():
+    while True:
+        bread_type = input("추가할 붕어빵을 선택하세요 (팥붕어빵, 슈크림붕어빵, 초코봉붕어빵) 똥는 '종료'입력 :")
+        if bread_type == "종료":
+            break
+        if bread_type in stock:
+            bread_count = int(input("추가할 개수를 입력하세요 : "))
+            stock[bread_type] += bread_count
+            print(f'{bread_type}의 재고가 {bread_count}개 추가되어 현재 {stock[bread_type]}개 입니다')
+        else:
+            print("올바른 메뉴 입력해주세요")
+
+#메인 기능 선택
 while True:
     mode = input("원하는 모드를 선택하세요 (주문, 관리자, 종료) : ")
     if mode =="종료":
